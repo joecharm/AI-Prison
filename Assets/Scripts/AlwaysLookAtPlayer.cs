54a9ef15744d0178e00cce0ec7b239bb47c748dc
Joe <919946@swansea.ac.uk>
2020-12-04 21:31:25 +0000

    Added character to the Security Guard NPC as well as a basic structure to the scene (walls & fence). Also added a script for the interactable NPCs to always face the player on the Y axis.

---
+++ Assets/Scripts/AlwaysLookAtPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysLookAtPlayer : MonoBehaviour
+{
+    // Set the transform of the target
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if target is defined
+        if(target != null)
+        {
+            // get the direction only on Y and change the look rotation of the GameObject the script is on to this direction
+            Vector3 dir = target.position - transform.position;
+            dir.y = 0;
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+    }
+}
