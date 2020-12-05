390b47a36a6fd623cbd02158ae36e681a5463e1f
Joe <919946@swansea.ac.uk>
2020-12-05 18:25:19 +0000

    Added raw animations of waving, asking question and turning head. These animations are triggered through Yarn at certain points in the dialogue, the trigger happens through a custom C# script which takes the name of the animation and plays it with a crossfade.

---
+++ Assets/Scripts/YarnAnimationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnAnimationController : MonoBehaviour
+{
+    public Animator animationController;
+
+    [YarnCommand("ToggleAnimation")]
+    public void ToggleAnimation(string AnimationName)
+    {
+        animationController.CrossFadeInFixedTime(AnimationName, 0.4f);
+    }
+
+}
