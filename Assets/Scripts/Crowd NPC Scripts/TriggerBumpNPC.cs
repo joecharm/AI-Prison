140572109e3e34619a3c8afb8dfcd97caa265cb5
Joe <919946@swansea.ac.uk>
2020-12-08 22:56:46 +0000

    Added feature where the crowd NPC will throw a punch of the NPC is knocked into two times by the player. The Hook punch animation was added and scene updated.

--- Assets/Scripts/Crowd NPC Scripts/TriggerBumpNPC.cs
+++ Assets/Scripts/Crowd NPC Scripts/TriggerBumpNPC.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class TriggerBumpNPC : MonoBehaviour
 {
+    // The animator on the NPC model
     public Animator CrowdAnimator;
+    private int count = 0;
 
+    // On trigger enter of the collider
     private void OnTriggerEnter(Collider other)
     {
+        // if the tag of the collider does not include Player
         if(other.CompareTag("Player"))
         {
-            CrowdAnimator.SetBool("TriggerBumpNPC", true);
+
+            // iterate on count to track number of times of collision
+            count++;
+
+            // if count is greater than 2, set throw punch to True
+            if(count > 2)
+            {
+                CrowdAnimator.SetBool("ThrowPunch", true);
+            }
+
+            // Set the boolean to true which triggers the TriggerBumpNPC (causing the angry animation) - only triggers when punch is not thrown
+            if (CrowdAnimator.GetBool("ThrowPunch") == false)
+            {
+                CrowdAnimator.SetBool("TriggerBumpNPC", true);
+            }
+
         }
         
     }
 
+    // When the player leaves the collider
     private void OnTriggerExit(Collider other)
     {
+        // if the tag of the collider does not include Player
         if (other.CompareTag("Player"))
         {
+            // Stop the animation that was triggered above
             CrowdAnimator.SetBool("TriggerBumpNPC", false);
+            CrowdAnimator.SetBool("ThrowPunch", false);
         }
 
     }
---
+++ Assets/Scripts/Crowd NPC Scripts/TriggerWaveNPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWaveNPC : MonoBehaviour
+{
+    public Animator CrowdAnimator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CrowdAnimator.SetBool("Wave", true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CrowdAnimator.SetBool("Wave", false);
+        }
+
+    }
+}
