a0079ad429def20dea97b0eb5b82925c8867f199
Joe <919946@swansea.ac.uk>
2020-12-08 21:44:19 +0000

    The CrowdNPC Animation Controller has been updated to include a new animation (with blend tree) of being angry. This is triggered through a C# script when the player enters the NPCs collider trigger.

---
+++ Assets/Scripts/Crowd NPC Scripts/TriggerBumpNPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBumpNPC : MonoBehaviour
+{
+    public Animator CrowdAnimator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            CrowdAnimator.SetBool("TriggerBumpNPC", true);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CrowdAnimator.SetBool("TriggerBumpNPC", false);
+        }
+
+    }
+}
