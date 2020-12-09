ebe445acb0ca4e82f1d40d0c35ae4b1d1c2aa1de
Joe <919946@swansea.ac.uk>
2020-12-09 21:02:29 +0000

    Added a random chance than the NPC will be offensive idle on first load. If above the 0.5 threshold the NPC will be offensive idle, if NPC sad threshold is above 0.7 the NPC will be sad instead.

--- Assets/Scripts/Crowd NPC Scripts/IsNPCOffensiveIdle.cs
+++ Assets/Scripts/Crowd NPC Scripts/IsNPCOffensiveIdle.cs
@@ -2,42 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IsNPCSad : MonoBehaviour
+public class IsNPCOffensiveIdle : MonoBehaviour
 {
     // Make the player play a sad idle state at random when idle (50% chance as defined in the animator condition)
 
     // The Crowd NPC Animator
     public Animator animator;
     // Boolean of if the NPC has been sad before
-    private bool hasBeenSad = false;
+    private bool hasBeenOffensive = false;
     // a float chance of the NPC being sad
-    private float sadChance;
+    private float offensiveChance;
 
     private void Start()
     {
         // call the set method
-        isNPCSad();
+        isNPCOffensive();
     }
 
-    public void isNPCSad()
+    public void isNPCOffensive()
     {
         // if the NPC has not been sad before, generate a random num between 0-1.
-        if (hasBeenSad == false)
+        if (hasBeenOffensive == false)
         {
             // generate random number 
-            sadChance = Random.Range(0.0f, 1.0f);
+            offensiveChance = Random.Range(0.0f, 1.0f);
             // set the float in the animator parameter
-            animator.SetFloat("IsSadChance", sadChance);
+            animator.SetFloat("IsOffensiveChance", offensiveChance);
 
-            // if the sad chance is above 0.7
-            if (sadChance >= 0.7f)
+            // if the sad chance is above 0.5
+            if (offensiveChance >= 0.5f)
             {
                 // set hasBeenSad to true
-                hasBeenSad = true;
+                hasBeenOffensive = true;
                 // set the boolean parameter of hasBeenSad to true
-                animator.SetBool("hasBeenSad", true);
+                animator.SetBool("hasBeenOffensive", true);
                 // Play the animator state one time
-                animator.Play("SadIdle");
+                animator.Play("OffensiveIdle");
             }
 
         }
