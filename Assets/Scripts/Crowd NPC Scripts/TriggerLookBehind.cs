535c74eb1a16e10cca68d2414c15061123c9a11e
Joe <919946@swansea.ac.uk>
2020-12-10 10:25:55 +0000

    Added a new player interaction so the NPC will look behind if the player walks behind them within a 2m radius. This can be triggered when the NPC is at any idle state and when walking.

---
+++ Assets/Scripts/Crowd NPC Scripts/TriggerLookBehind.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLookBehind : MonoBehaviour
+{
+    // The animator on the NPC model
+    public Animator CrowdAnimator;
+
+    // On trigger enter of the collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // if the tag of the collider does not include Player
+        if(other.CompareTag("Player"))
+        {
+            // set the trigger of look behind which will play the animation based on the condition in the animator
+            CrowdAnimator.SetTrigger("LookBehind");
+
+        }
+        
+    }
+
+}
